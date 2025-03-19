0dd816f44c109d6e1d33084c6620e121882e9e82
Sayuru Vihagana <127166019+vihagana99@users.noreply.github.com>
2025-03-19 23:31:41 +0530

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -3,6 +3,8 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, collection, getDoc, doc, setDoc } from 'firebase/firestore'; // Add setDoc import
 import { getStorage } from 'firebase/storage';
 
+// Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,6 +14,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const fs = getFirestore(app);
