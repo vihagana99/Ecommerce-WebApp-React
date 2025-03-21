2fac74bd0d730898c2627e1d87c8cf6f6607d9b4
Sayuru Vihagana <127166019+vihagana99@users.noreply.github.com>
2025-03-21 23:07:08 +0530

    update

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -7,14 +7,14 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDR_uCAp4b8uCOp_LN6ccER6ivUBYU21yw",
+  authDomain: "e-commerce-react-31670.firebaseapp.com",
+  databaseURL: "https://e-commerce-react-31670-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "e-commerce-react-31670",
+  storageBucket: "e-commerce-react-31670.firebasestorage.app",
+  messagingSenderId: "684052674370",
+  appId: "1:684052674370:web:7c02323a1370ecc900fadb",
+  measurementId: "G-416G3QNN60"
 };
 
 const app = initializeApp(firebaseConfig);
